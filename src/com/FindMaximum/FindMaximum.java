a47150bb23bfa32da9954ecf961322647aa5b6af
balu-pinisety <manikantapandu225@gmail.com>
2021-06-28 20:30:46 +0530

    Refactor: Code to Generic class to find maximum

--- src/com/FindMaximum/FindMaximum.java
+++ src/com/FindMaximum/FindMaximum.java
@@ -5,7 +5,43 @@ package com.FindMaximum;
  * To find the maximum among different values by using Variables
  * Variables are of Wrapper Class Integer, Double and String type
  */
-public class FindMaximum {
+public class FindMaximum<T extends Comparable<T>> {
+	private T firstValue, secondValue, thirdValue;
+	
+	public FindMaximum(T firstInput, T secondInput, T thirdInput) {
+		this.firstValue = firstInput;
+		this.secondValue = secondInput;
+		this.thirdValue = thirdInput;
+	}
+	
+	public T getMaximum() {
+		return FindMaximum.getMaximum(firstValue, secondValue, thirdValue);
+	}
+
+	/** 
+	 * Method to find Maximum Using Generic
+	 * Ability to take values by variables which are Wrapper class Type  
+	 * To find the maximum of values using compareTo method
+	 * Prints the maximum value
+	 * @param x, y, z
+	 * @return 
+	 */
+	private static <T extends Comparable<T>> T getMaximum(T x, T y, T z) {
+		T max=x;//Initializing the Variable
+		if (y.compareTo(x)>0) {
+			max=y;
+		}
+		if (z.compareTo(max)>0) {
+			max=z;
+		}
+		printOut(x, y, z, max);
+		return max;
+	}
+	
+	private static <T> void printOut(T i, T j, T k, T big) {
+		//Printing the max value
+		System.out.printf("Maximum of %s, %s and %s is %s\n",i,j,k,big);
+	}
 
 	/**
 	 * To assign the values to Wrapper class type Variables 
@@ -19,27 +55,9 @@ public class FindMaximum {
 		Float l=33.48f, m=68.76f, n=45.72f;
 		String p="apple",q="peach",r="banana";
 		// Calling method to mind maximum
-		getMaximum(a,b,c);
-		getMaximum(l,m,n);
-		getMaximum(p,q,r);
-	}
-
-	/** 
-	 * Method to find Maximum Using Generic
-	 * Ability to take values by variables which are Wrapper class Type  
-	 * To find the maximum of values using compareTo method
-	 * Prints the maximum value
-	 * @param x, y, z
-	 */
-	private static <T extends Comparable> void getMaximum(T x, T y, T z) {
-		T max=x;//Initializing the Variable
-		if (y.compareTo(x)>0) {
-			max=y;
-		}
-		if (z.compareTo(max)>0) {
-			max=z;
-		}
-		System.out.println("Maximum of "+x+", "+y+", "+z+" is "+max);//Printing the max value
+		new FindMaximum(a,b,c).getMaximum();
+		new FindMaximum(l,m,n).getMaximum();
+		new FindMaximum(p,q,r).getMaximum();
 	}
 	
 }
