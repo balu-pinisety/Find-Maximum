190ed397d8e96b0b2450867a77c52e59b7f1bf47
balu-pinisety <manikantapandu225@gmail.com>
2021-06-28 17:08:47 +0530

    Added: File for creation of FindMaximum code

---
+++ README.md
@@ -0,0 +1,2 @@
+# Find Maximum
+To find the maximum among different values by using Variables which are of Wrapper Class Integer, Double and String type.
---
+++ src/com/FindMaximum/FindMaximum.java
@@ -0,0 +1,10 @@
+package com.FindMaximum;
+
+public class FindMaximum {
+
+	public static void main(String[] args) {
+		//To display Welcome Message
+		System.out.println("Program To find the Maximum among different values");
+	}
+	
+}
